071ee1b5762e7624978401a6f6905f9f9210b0e7
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2024-03-17 08:06:59 +0500

    first commit

---
+++ script1.js
@@ -0,0 +1,44 @@
+
+// Smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuIcon = document.getElementById('menu-icon');
+    var navbar = document.getElementById('navbar');
+    var topButton = document.querySelector('.top-button');
+    var sections = document.querySelectorAll('.section');
+
+    menuIcon.addEventListener('click', function() {
+        var ul = navbar.querySelector('ul');
+        if (ul.style.display === 'none' || ul.style.display === '') {
+            ul.style.display = 'flex';
+            moveElementsDown();
+        } else {
+            ul.style.display = 'none';
+            moveElementsUp();
+        }
+    });
+
+    function moveElementsDown() {
+        topButton.style.marginTop = navbar.offsetHeight + 'px';
+        sections.forEach(function(section) {
+            section.style.marginTop = navbar.offsetHeight + 'px';
+        });
+    }
+
+    function moveElementsUp() {
+        topButton.style.marginTop = '0';
+        sections.forEach(function(section) {
+            section.style.marginTop = '40px'; // Adjust as needed
+        });
+    }
+});
+
